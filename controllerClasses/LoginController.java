45c1abb7b5c78402dfd31314ce6865383312fbe4
FrankAnder <92362741+FrankAnder@users.noreply.github.com>
2021-10-20 16:49:50 +0200

    Add files via upload

---
+++ controllerClasses/LoginController.java
@@ -0,0 +1,51 @@
+package com.kollector.app.controller;
+
+import com.kollector.app.global.GlobalData;
+import com.kollector.app.model.User;
+import com.kollector.app.model.Role;
+import com.kollector.app.repository.RoleRepository;
+import com.kollector.app.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+public class LoginController {
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+    @Autowired
+    UserRepository userRepository;
+    @Autowired
+    RoleRepository roleRepository;
+
+    @GetMapping("/login")  //go to login html
+    public String login(){
+        GlobalData.cart.clear();  //clear cart when user logs in
+        return "login";
+    }
+
+    @GetMapping("/register")
+    public String registerGet(){
+        return "register";
+    }
+    @PostMapping("/register")
+    public String registerPost(@ModelAttribute("user") User user, HttpServletRequest request) throws ServletException{
+        String password = user.getPassword();
+        user.setPassword(bCryptPasswordEncoder.encode(password));
+        List<Role> roles = new ArrayList<>();
+        roles.add(roleRepository.findById(2).get());
+        user.setRoles(roles);
+        userRepository.save(user);
+        request.login(user.getEmail(), password);
+        return "redirect:/";
+    }
+
+}
