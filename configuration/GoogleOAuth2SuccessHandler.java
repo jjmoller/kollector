7717703ced24af604dd0e9c03a78899d6e081ef5
FrankAnder <92362741+FrankAnder@users.noreply.github.com>
2021-10-20 16:52:11 +0200

    Add files via upload

---
+++ configuration/GoogleOAuth2SuccessHandler.java
@@ -0,0 +1,53 @@
+package com.kollector.app.configuration;
+
+
+import com.kollector.app.model.User;
+import com.kollector.app.model.Role;
+import com.kollector.app.repository.RoleRepository;
+import com.kollector.app.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.security.web.DefaultRedirectStrategy;
+import org.springframework.security.web.RedirectStrategy;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class GoogleOAuth2SuccessHandler implements AuthenticationSuccessHandler {
+    @Autowired
+    RoleRepository roleRepository;
+    @Autowired
+    UserRepository userRepository;
+
+    private RedirectStrategy redirectStrategy = new DefaultRedirectStrategy();
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Authentication authentication) throws IOException, ServletException {
+        OAuth2AuthenticationToken token = (OAuth2AuthenticationToken) authentication;
+        String email = token.getPrincipal().getAttributes().get("email").toString();
+        if(userRepository.findUserByEmail(email).isPresent()){
+        }else{
+            User user = new User();
+            user.setFirstName(token.getPrincipal().getAttributes().get("given_name").toString());
+            user.setLastName(token.getPrincipal().getAttributes().get("family_name").toString());
+            user.setEmail(email);
+            List<Role> roles = new ArrayList<>();
+            roles.add(roleRepository.findById(2).get());
+            user.setRoles(roles);
+            userRepository.save(user);
+
+        }
+        redirectStrategy.sendRedirect(httpServletRequest, httpServletResponse, "/");
+    }
+
+
+
+    }
+
---
+++ configuration/SecurityConfig.java
@@ -0,0 +1,68 @@
+package com.kollector.app.configuration;
+
+import com.kollector.app.service.CustomUserDetailService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Autowired
+    GoogleOAuth2SuccessHandler googleOAuth2SuccessHandler;
+    @Autowired
+    CustomUserDetailService customUserDetailService;
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/","/shop/**","/register", "/h2-console/**").permitAll()
+                .antMatchers("/admin/**").hasRole("ADMIN")
+                .anyRequest()
+                .authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .permitAll()
+                .failureUrl("/login?error=true")
+                .defaultSuccessUrl("/")
+                .usernameParameter("email")
+                .passwordParameter("password")
+                .and()
+                .oauth2Login()
+                .loginPage("/login")
+                .successHandler(googleOAuth2SuccessHandler)
+                .and()
+                .logout()
+                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                .logoutSuccessUrl("/login")
+                .invalidateHttpSession(true)
+                .deleteCookies("JSESSIONID")
+                .and()
+                .exceptionHandling()
+                .and()
+                .csrf()
+                .disable();
+        http.headers().frameOptions().disable();
+    }@Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(customUserDetailService);
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/resources/**","/static/**","images/**","productImages/**","/css/**","/js/**");
+    }
+}
