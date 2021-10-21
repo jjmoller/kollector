2f79a19f53c724a207d115be4a2ca86ec5c90ab7
JJ Möller <juanmoller.jm@gmail.com>
2021-10-21 18:06:45 +0200

    Add files via upload

---
+++ src/test/java/com/kollector/app/MajorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kollector.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MajorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
