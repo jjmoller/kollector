caac8ff6c36cbcc2937b9ec04c58515aa15403e7
FrankAnder <92362741+FrankAnder@users.noreply.github.com>
2021-10-21 11:08:43 +0200

    Add files via upload

---
+++ src/main/java/com/kollector/app/model/CustomUserDetail.java
@@ -0,0 +1,53 @@
+package com.kollector.app.model;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+public class CustomUserDetail extends User implements UserDetails {
+    public CustomUserDetail(User user){
+        super(user);
+    }
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        List<GrantedAuthority> authorityList = new ArrayList<>();
+        super.getRoles().forEach(role -> {
+            authorityList.add(new SimpleGrantedAuthority(role.getName()));
+        });
+        return authorityList;
+    }
+
+    @Override
+    public String getUsername() {
+        return super.getEmail();
+    }
+    @Override
+    public String getPassword() {
+        return super.getPassword();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
+
