72c3f0791b9efcfd542592100f121932ef9a437e
FrankAnder <92362741+FrankAnder@users.noreply.github.com>
2021-10-21 11:10:02 +0200

    Add files via upload

---
+++ src/main/java/com/kollector/app/model/Role.java
@@ -0,0 +1,22 @@
+package com.kollector.app.model;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+import java.util.List;
+
+@Data
+@Entity
+@Table(name = "roles")
+public class Role {
+
+    @Id@GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+    @Column(nullable = false, unique = true)
+    @NotEmpty
+    private String name;
+
+    @ManyToMany(mappedBy = "roles")
+    private List<User> users;
+}
