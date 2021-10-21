acabec479fad9c125684be71e38177e566844711
FrankAnder <92362741+FrankAnder@users.noreply.github.com>
2021-10-21 11:12:22 +0200

    Create CustomUserDetailService.java

---
+++ src/main/java/com/kollector/app/service/CustomUserDetailService.java
@@ -0,0 +1,24 @@
+package com.kollector.app.service;
+
+import com.kollector.app.repository.UserRepository;
+import com.kollector.app.model.CustomUserDetail;
+import com.kollector.app.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class CustomUserDetailService implements UserDetailsService {
+    @Autowired
+    UserRepository userRepository;
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        Optional<User> user = userRepository.findUserByEmail(email);
+        user.orElseThrow(() -> new UsernameNotFoundException("User not found"));
+        return user.map(CustomUserDetail::new).get();
+    }
+}
