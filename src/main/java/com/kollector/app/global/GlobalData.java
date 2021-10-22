2cf1b6aaba4dc264f6d14c362e99feeb396a12f3
FrankAnder <92362741+FrankAnder@users.noreply.github.com>
2021-10-22 11:54:05 +0200

    Update GlobalData.java

--- src/main/java/com/kollector/app/global/GlobalData.java
+++ src/main/java/com/kollector/app/global/GlobalData.java
@@ -5,6 +5,8 @@ import com.kollector.app.model.ProductSell;
 import java.util.ArrayList;
 import java.util.List;
 
+// Class that contributes to the cart functionality.
+
 public class GlobalData {
     public static List<ProductSell> cart;
     static {
