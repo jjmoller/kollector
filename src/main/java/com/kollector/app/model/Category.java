9a90a862a62f7fdef39ad951baedbce07a8380fb
FrankAnder <92362741+FrankAnder@users.noreply.github.com>
2021-10-22 11:58:29 +0200

    Update Category.java

--- src/main/java/com/kollector/app/model/Category.java
+++ src/main/java/com/kollector/app/model/Category.java
@@ -4,6 +4,8 @@ import lombok.Data;
 
 import javax.persistence.*;
 
+// Indicates the relavant stored category attributes. The category_id attribute was chosen to generate automatically for ease-of-use. This model is thus to be used in other relevant package classes and or interfaces.
+
 @Entity
 @Data
 public class Category {
