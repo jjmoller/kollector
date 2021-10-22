23bc43c3c6042dcd87eb87e2c300d8aaab555e8a
FrankAnder <92362741+FrankAnder@users.noreply.github.com>
2021-10-22 12:03:14 +0200

    Update CustomUserDetail.java

--- src/main/java/com/kollector/app/model/CustomUserDetail.java
+++ src/main/java/com/kollector/app/model/CustomUserDetail.java
@@ -8,6 +8,8 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
+// Indicates if the login requirements are granted access or not. Also indicates if the login credentials and or account holder's details are expired, locked and authorised.
+
 public class CustomUserDetail extends User implements UserDetails {
     public CustomUserDetail(User user){
         super(user);
