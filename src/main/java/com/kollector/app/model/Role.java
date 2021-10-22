3cd285b95f2a72ea970abba820c16d5e50e3daf9
FrankAnder <92362741+FrankAnder@users.noreply.github.com>
2021-10-22 12:11:15 +0200

    Update Role.java

--- src/main/java/com/kollector/app/model/Role.java
+++ src/main/java/com/kollector/app/model/Role.java
@@ -6,6 +6,9 @@ import javax.persistence.*;
 import javax.validation.constraints.NotEmpty;
 import java.util.List;
 
+// This class represents domain-specific data and business logic in the model view controller. Also indicates the relavant detail about the specific roles. The id attribute was chosen to generate automatically for ease-of-use. This model is thus to be used in other relevant package classes and or interfaces.
+// The many to one relationship is based on the fact that many users will belong to a single role.
+
 @Data
 @Entity
 @Table(name = "roles")
