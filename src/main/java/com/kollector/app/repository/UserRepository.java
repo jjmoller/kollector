796b792d3b151ac492758bce5d63eb720021ee83
JJ Möller <juanmoller.jm@gmail.com>
2021-10-22 12:03:58 +0200

    Update UserRepository.java

--- src/main/java/com/kollector/app/repository/UserRepository.java
+++ src/main/java/com/kollector/app/repository/UserRepository.java
@@ -5,6 +5,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
+//Repository for the user information
+
 public interface UserRepository extends JpaRepository<User, Integer> {
    Optional<User> findUserByEmail(String email);
 }
