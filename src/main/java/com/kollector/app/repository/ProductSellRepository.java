e92253d3a7697a5a6d8834d70ef96368e4ab5ab9
JJ Möller <juanmoller.jm@gmail.com>
2021-10-22 12:02:34 +0200

    Update ProductSellRepository.java

--- src/main/java/com/kollector/app/repository/ProductSellRepository.java
+++ src/main/java/com/kollector/app/repository/ProductSellRepository.java
@@ -5,6 +5,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
+//Products for sale repository
+
 public interface ProductSellRepository extends JpaRepository<ProductSell, Long> {
     List<ProductSell> findAllSellByCategory_Id(int id);
 
