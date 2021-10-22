4d08d0a26404447567c3407b3730582aebac5ce8
JJ Möller <juanmoller.jm@gmail.com>
2021-10-22 12:02:03 +0200

    Update ProductRepository.java

--- src/main/java/com/kollector/app/repository/ProductRepository.java
+++ src/main/java/com/kollector/app/repository/ProductRepository.java
@@ -5,6 +5,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
+//Catalogue/Product repository
+
 public interface ProductRepository extends JpaRepository<Product, Long> {
     List<Product> findAllByCategory_Id(int id);
 
