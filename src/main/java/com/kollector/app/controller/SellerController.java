0c8a594ecb8f16d04a512cb6c85bbe59e4b47634
JJ Möller <juanmoller.jm@gmail.com>
2021-10-22 11:59:46 +0200

    Update SellerController.java

--- src/main/java/com/kollector/app/controller/SellerController.java
+++ src/main/java/com/kollector/app/controller/SellerController.java
@@ -19,6 +19,7 @@ import java.util.Optional;
 
 //Controller class for seller page
 //Methods similar to the admim controller.
+//Methods for handling HTTP post and get requests with the provided URI expression.
 
 @Controller
 public class SellerController {
