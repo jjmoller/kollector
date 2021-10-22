404226f3e646e533c597a23c386fce6ca4cb472b
JJ Möller <juanmoller.jm@gmail.com>
2021-10-22 12:03:15 +0200

    Update RoleRepository.java

--- src/main/java/com/kollector/app/repository/RoleRepository.java
+++ src/main/java/com/kollector/app/repository/RoleRepository.java
@@ -3,5 +3,7 @@ package com.kollector.app.repository;
 import com.kollector.app.model.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+//Repository for the 2 roles
+
 public interface RoleRepository extends JpaRepository<Role, Integer> {
 }
