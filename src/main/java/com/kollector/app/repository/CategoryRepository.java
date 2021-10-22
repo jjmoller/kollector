d9ecc61b89e22244d1811ff10bfeeba56f9a9524
JJ Möller <juanmoller.jm@gmail.com>
2021-10-22 12:01:32 +0200

    Update CategoryRepository.java

--- src/main/java/com/kollector/app/repository/CategoryRepository.java
+++ src/main/java/com/kollector/app/repository/CategoryRepository.java
@@ -3,6 +3,8 @@ package com.kollector.app.repository;
 import com.kollector.app.model.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+//Categories repository 
+
 public interface CategoryRepository extends JpaRepository<Category, Integer> {
 
 }
