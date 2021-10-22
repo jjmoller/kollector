c1d5edc751649ee16a08b63dff1685e3efc6f8bd
FrankAnder <92362741+FrankAnder@users.noreply.github.com>
2021-10-22 12:09:29 +0200

    Update ProductSell.java

--- src/main/java/com/kollector/app/model/ProductSell.java
+++ src/main/java/com/kollector/app/model/ProductSell.java
@@ -4,6 +4,9 @@ import lombok.Data;
 
 import javax.persistence.*;
 
+// This class represents domain-specific data and business logic in the model view controller. Also indicates the relavant stored attributes of the cards that are being sold. The id attribute was chosen to generate automatically for ease-of-use. This model is thus to be used in other relevant package classes and or interfaces.
+// The many to one relationship is based on the fact that many cards will belong to a single card category.
+
 @Entity
 @Data
 public class ProductSell {
